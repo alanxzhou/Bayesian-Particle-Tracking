835d63435c1e2674fec2a91112c2b2203af3a63b
alanzhou93 <alanzhou93@gmail.com>
2016-04-12 09:47:35 -0400

    Edit to likelihood
    Fixed likelihood to subtract consecutive points from each other in the
    data to produce the likelihood

--- model.py
+++ model.py
@@ -61,7 +61,17 @@ def likelihood(data, sigma, mu, a, T = 300, center = 0, tau = 1):
 	kb = 1.38*10**(-23)
 	D = (kb*T)/(6*np.pi*mu*a)
 	sigma1 = np.sqrt(3*D*tau)
-	predicted = model(sigma, mu, a, T = T, center = center, tau = tau)
+	#predicted = model(sigma, mu, a, T = T, center = center, tau = tau)
 
-	return (2*np.pi)**(-len(data)/2)*np.prod((sigma1**2+sigma**2)**(-1/2))*np.exp((-((data-predicted)**2)/(2*(sigma1**2+sigma**2))).sum())
+	#This should give us the delta x_{i,i-1}. point_before is just the data list minus the last element.
+	#data_points is the data list minus the first element.
+	point_before = list(old_list)
+	point_before.pop[len(point_before)]
+	point_before = np.array(point_before)
+	data_points = list(data)
+	data_points.pop[0]
+	data_points = np.array(data_points)
+
+
+	return (2*np.pi)**(-len(data)/2)*np.prod((sigma1**2+sigma**2)**(-1/2))*np.exp((-((data_points-point_before)**2)/(2*(sigma1**2+sigma**2))).sum())
 
