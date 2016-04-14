d234c763061484552f33c52f5dbf6a457957d991
alanzhou93 <alanzhou93@gmail.com>
2016-04-14 01:32:40 -0400

    Moving model to correct directory

--- Bayesian_Particle_Tracking/model.py
+++ Bayesian_Particle_Tracking/model.py
@@ -1,30 +1,34 @@
 import numpy as np
 from numpy.random import normal
 from numpy.random import uniform
-import matplotlib as plt
 import scipy
 
-def model(nsteps, sigma, mu, a, T = 300, nwalkers = 1, center = 0, tau = 1):
+def model(nsteps, sigma, mu, a, initial_coordinate, T = 300, nwalkers = 1, center = 0, tau = 1):
 	"""
 	This function provides the trajectory for a 3D diffusion process.
-	Returns trajectories in (x,y,z) coordiantes as column vectors
+	Returns trajectories in (x,y,z) coordinates as column vectors
 
 	Parameters:
 		nsteps: number of steps to take
 		sigma: uncertainty in positional data; is a function of parameters in particle tracking in raw data
 		mu: viscosity of medium
 		a: radius of particle
+		initial_coordinate: initial coordinate of particle in array [x,y,z]
 		T: temperature (default at room temperature ~ 300K)
 		tau: time constant (default at 1s)
 		nwalkers: number of walkers (NOTE: not implemented yet)
 
-	Note that D is the diffusion constant and is a funciton of a, mu, T
+	Note that D is the diffusion coefficient and is a function of a, mu, T
 	"""
 	
+	x_start = normal(initial_coordinate[0],sigma*np.sqrt(3))
+	y_start = normal(initial_coordinate[1],sigma*np.sqrt(3))
+	z_start = normal(initial_coordinate[2],sigma*np.sqrt(3))
+
 	kb = 1.38*10**(-23)
 	D = (kb*T)/(6*np.pi*mu*a)
 	sigma1 = np.sqrt(3*D*tau)
-	r = abs(normal(center, np.sqrt(sigma1**2+sigma**2), nsteps))/2
+	r = abs(normal(center, sigma1, nsteps))/2
 	theta = uniform(0,np.pi,nsteps)
 	phi = uniform(0,2*np.pi,nsteps)
 	#Spherical to Cartesian Coordinates
@@ -32,9 +36,9 @@ def model(nsteps, sigma, mu, a, T = 300, nwalkers = 1, center = 0, tau = 1):
 	y = r*np.sin(theta)*np.sin(phi)
 	z = r*np.cos(theta)
 	#Trajectories
-	traj_x = np.cumsum(x)
-	traj_y = np.cumsum(y)
-	traj_z = np.cumsum(z)
+	traj_x = np.cumsum(x) + x_start
+	traj_y = np.cumsum(y) + y_start
+	traj_z = np.cumsum(z) + z_start
 	return(np.array((traj_x, traj_y, traj_z)).T)
 
 def likelihood(data, sigma, mu, a, T = 300, center = 0, tau = 1):
@@ -55,14 +59,19 @@ def likelihood(data, sigma, mu, a, T = 300, center = 0, tau = 1):
 	D = (kb*T)/(6*np.pi*mu*a)
 	sigma1 = np.sqrt(3*D*tau)
 
+	#Turn data into displacements instead of positions
+	data = data-data[0]
+
 	#This should give us the delta x_{i,i-1}. point_before is just the data list minus the last element.
 	#data_points is the data list minus the first element.
 	#point_before is the data list minus the last element.
 	#distance is the measurement of the distance between two consecutive points in the series.
+
 	point_before = list(data)
 	del point_before[len(point_before)-1]
 	point_before = np.array(point_before)
 	x_before, y_before, z_before = point_before[:,0], point_before[:,1], point_before[:,2]
+
 	data_points = list(data)
 	del data_points[0]
 	data_points = np.array(data_points)
