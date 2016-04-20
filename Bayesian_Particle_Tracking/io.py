c901f7df07e618d00a98247b9c87f92531c0e280
Tom Dimiduk <tom@dimiduk.net>
2016-04-20 10:33:16 -0400

    replace machine specific data path with general function

--- Bayesian_Particle_Tracking/io.py
+++ Bayesian_Particle_Tracking/io.py
@@ -1,13 +1,27 @@
 from Bayesian_Particle_Tracking import model
 import numpy as np
+import os
 
 #T=300, mu = 10^-4, a = 10^-8
 #=> D = 2.19*10^-10
+# TODO: FIXME: You generally don't want to be running actual computations in files that are imported. Move this data generation to a seperate script you run explicitly
 data = model.generator(1000,10**(-8),10**(-4),10**(-8),[0,0,0], T=300)
 np.save('test_data', data)
 
-#This section is specific to my computer: to be removed.
-home_dir = "/Users/alanzhou/Documents/Physics_201/final_project/Bayesian-Particle-Tracking/Bayesian_Particle_Tracking/"
-new_data = np.load(home_dir + 'test_data.npy')
+def get_example_data_file_path(filename, data_dir='example_data'):
+    # __file__ is the location of the source file currently in use (so
+    # in this case io.py). We can use it as base path to construct
+    # other paths from that should end up correct on other machines or
+    # when the package is installed
+    start = os.path.abspath(__file__)
+    start_dir = os.path.dirname(start)
+    # If you need to go up another directory (for example if you have
+    # this function in your tests directory and your data is in the
+    # package directory one level up) you can use
+    # up_dir = os.path.split(start_dir)[0]
+    data_dir = os.path.join(start_dir, data_dir)
+    return os.path.join(start_dir, data_dir, filename)
+
+new_data = np.load(get_example_data_file_path('test_data.npy', ''))
 
 input_data = model.diffusion(new_data)
